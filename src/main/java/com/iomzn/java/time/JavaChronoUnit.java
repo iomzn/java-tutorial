007f512dbc542db73d3bd3b19cea1a5a260d777d
zhangjian <zhangjian225417@qq.com>
2017-12-08 14:34:05 +0800

    fix

---
+++ src/main/java/com/iomzn/java/time/JavaChronoUnit.java
@@ -0,0 +1,42 @@
+package com.iomzn.java.time;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class JavaChronoUnit {
+
+    public static void main(String[] args) {
+        // instance
+        ChronoUnit v01 = ChronoUnit.NANOS; // TimeBased|PT0.000000001S
+        ChronoUnit v02 = ChronoUnit.MICROS; // TimeBased|PT0.000001S
+        ChronoUnit v03 = ChronoUnit.MILLIS; // TimeBased|PT0.001S
+        ChronoUnit v04 = ChronoUnit.SECONDS; // TimeBased|PT1S
+        ChronoUnit v05 = ChronoUnit.MINUTES; // TimeBased|PT1M
+        ChronoUnit v06 = ChronoUnit.HOURS; // TimeBased|PT1H
+        ChronoUnit v07 = ChronoUnit.HALF_DAYS; // TimeBased|PT12H
+        ChronoUnit v08 = ChronoUnit.DAYS; // DateBased|Estimated|PT24H
+        ChronoUnit v09 = ChronoUnit.WEEKS; // DateBased|Estimated|PT168H
+        ChronoUnit v10 = ChronoUnit.MONTHS; // DateBased|Estimated|PT730H29M6S
+        ChronoUnit v11 = ChronoUnit.YEARS; // DateBased|Estimated|PT8765H49M12S
+        ChronoUnit v12 = ChronoUnit.DECADES; //DateBased|Estimated|PT87658H12M
+        ChronoUnit v13 = ChronoUnit.CENTURIES; // DateBased|Estimated|PT876582H
+        ChronoUnit v14 = ChronoUnit.MILLENNIA; // DateBased|Estimated|PT8765820H
+        ChronoUnit v15 = ChronoUnit.ERAS; // DateBased|Estimated|PT8765820000000H
+        ChronoUnit v16 = ChronoUnit.FOREVER; // Estimated|PT2562047788015215H30M7.999999999S
+
+        ChronoUnit chronoUnit = ChronoUnit.DAYS;
+
+        // getter
+        Duration duration = chronoUnit.getDuration(); // PT24H
+
+        // condition
+        boolean isDateBased = chronoUnit.isDateBased(); // true
+        boolean isTimeBased = chronoUnit.isTimeBased(); // false
+        boolean isDurationEstimated = chronoUnit.isDurationEstimated(); // true
+        boolean isSupportedBy = chronoUnit.isSupportedBy(LocalDate.now()); // true
+
+        // calculate
+        long daysBetween = chronoUnit.between(LocalDate.of(1991, 2, 25), LocalDate.now()); // demo: 9783
+    }
+}
--- src/main/java/com/iomzn/java/time/JavaInstant.java
+++ src/main/java/com/iomzn/java/time/JavaInstant.java
@@ -1,7 +1,10 @@
 package com.iomzn.java.time;
 
 import java.time.Clock;
+import java.time.Duration;
 import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class JavaInstant {
 
@@ -14,10 +17,24 @@ public class JavaInstant {
         // instance
         Instant v01 = Instant.now(); // demo: 2017-12-07T09:11:44.084799Z
         Instant v02 = Instant.now(Clock.systemUTC()); // demo: 2017-12-07T10:28:15.029701Z
-        Instant v03 = Instant.now();
+        Instant v03 = Instant.ofEpochMilli(1); // 1970-01-01T00:00:00.001Z
+        Instant v04 = Instant.ofEpochSecond(1); // 1970-01-01T00:00:01Z
+        Instant v05 = Instant.ofEpochSecond(1, 1); // 1970-01-01T00:00:01.000000001Z
+        Instant v06 = Instant.from(ZonedDateTime.now()); // demo: 2017-12-07T11:04:48.612724Z
+        Instant v07 = Instant.parse("1970-01-01T00:00:00Z"); // 1970-01-01T00:00:00Z
+
+        Instant instant = Instant.EPOCH; // 1970-01-01T00:00:00Z
+
+        // plus|minus
+        Instant p01 = instant.plusNanos(1); // 1970-01-01T00:00:00.000000001Z
+        Instant p02 = instant.plusMillis(1); // 1970-01-01T00:00:00.001Z
+        Instant p03 = instant.plusSeconds(1); // 1970-01-01T00:00:01Z
+        Instant p04 = instant.plus(Duration.ofSeconds(1)); // 1970-01-01T00:00:01Z
+        Instant p05 = instant.plus(1, ChronoUnit.SECONDS); // 1970-01-01T00:00:01Z ~ []
 
 
-        System.out.println(v02);
+
+
 
 
 
